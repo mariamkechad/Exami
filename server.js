daf05c7c2c97d515cdf4c9d9180fba3f916aef14
DOUAE-001 <douae.tamestast@etu.uae.ac.ma>
2025-04-22 16:51:07 +0100

    change serverjs

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const port = 3000;
 
-app.get("/api/", (req, res) => {
-  console.log("Bonjour, Mars");
-});
-
-app.post("/api/login", (req, res) => {
-  // TODO: (douae) Login endpoint.
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.post("/api/signup", (req, res) => {
   // TODO: (ali) Siginup endpoint.
